aa0a4d84bafc9a806c97f37625a1c6d4a6ac1ffb
Talha836902 <talhaasifmf786@gmail.com>
2023-11-23 17:27:43 +0500

    all files add

---
+++ app.js
@@ -0,0 +1,56 @@
+// let data ="Hello";
+// let data2 =" Student";
+
+// let data3 = data + data2; //Strings Concatenation
+// console.log(data3);
+
+// num + num = addition
+// num + string = Concatenation
+// string + num = Concatenation
+// string + string = Concatenation
+
+// let data = 5;
+// let data2 = 5;
+
+// let data3 = data + data2; //addition
+// console.log(data3);
+
+// let data ="Hello Students";
+// let data2 = data.includes("Students");
+// console.log(data2);
+
+//Math Operators = + - * / %
+
+// let data = 10;
+// let data2 = 3;
+// console.log(Number(data) % data2);
+
+// let userFavNum = prompt("what is your favorite number?");
+// console.log(userFavNum);
+
+// let userName = prompt("what is your Name?");
+// document.write("Welcome to my website "+userName);
+
+// let randomNumber = Math.random() ;
+// //randomNumber =randomNumber * 10;
+// randomNumber *= 10; // Short Hand
+// let roundedNumber = Math.round(randomNumber);
+// console.log(roundedNumber);
+
+
+//Example: BODMAS = Brackets of, Divide, Multiply, Addition, Subtraction
+let num = 5 ** 3;  //power
+
+console.log(num);
+
+
+
+
+
+
+
+
+
+
+
+
